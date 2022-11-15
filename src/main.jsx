ac593b3e72e9d29b56bf4d601fe7f814c27c24a2
“Henry-Hiles” <henry@henryhiles.com>
2022-11-15 11:42:35 -0500

    Responsiveness change

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>
