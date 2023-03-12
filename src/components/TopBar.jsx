e4821889d2f85e69037cb1bcdc29454739509c55
Henry-Hiles <henry@henryhiles.com>
2023-03-12 14:12:45 -0400

    topbar improvements

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,46 +3,46 @@ import { Link } from "react-router-dom"
 import styles from "styles/TopBar.module.css"
 
 const TopBar = ({ search, setSearch, absolute }) => {
-    const [width, setWidth] = useState(window.innerWidth)
+	const [width, setWidth] = useState(window.innerWidth)
 
-    useEffect(() => {
-        const handleResize = () => setWidth(window.innerWidth)
+	useEffect(() => {
+		const handleResize = () => setWidth(window.innerWidth)
 
-        window.addEventListener("resize", handleResize)
+		window.addEventListener("resize", handleResize)
 
-        return () => window.removeEventListener("resize", handleResize)
-    }, [])
+		return () => window.removeEventListener("resize", handleResize)
+	}, [])
 
-    const Search = (
-        <input
-            type="text"
-            className={styles.Search}
-            value={search}
-            autoFocus
-            placeholder="Search for movies..."
-            onChange={({ target }) => setSearch(target.value)}
-        />
-    )
+	const Search = (
+		<input
+			type="text"
+			className={styles.Search}
+			value={search}
+			autoFocus
+			placeholder="Search for movies..."
+			onChange={({ target }) => setSearch(target.value)}
+		/>
+	)
 
-    return (
-        <div
-            className={`${styles.Container} ${
-                absolute && width > 1000 ? styles.Absolute : ""
-            } ${setSearch ? styles.HasSearch : ""}`}
-        >
-            <Link to="/">
-                <h1 className={styles.Title}>React Movie Finder</h1>
-            </Link>
+	return (
+		<div
+			className={`${styles.Container} ${
+				absolute ? styles.Absolute : ""
+			} ${setSearch ? styles.HasSearch : ""}`}
+		>
+			<Link to="/" className={styles.Title}>
+				<h1>React Movie Finder</h1>
+			</Link>
 
-            {setSearch && width > 1000 && Search}
+			{setSearch && width > 1000 && Search}
 
-            <Link to="/ratings" className={styles.Ratings}>
-                <h1>Your ratings</h1>
-            </Link>
+			<Link to="/ratings" className={styles.Ratings}>
+				<h1>Your ratings</h1>
+			</Link>
 
-            {setSearch && width <= 1000 && Search}
-        </div>
-    )
+			{setSearch && width <= 1000 && Search}
+		</div>
+	)
 }
 
 export default TopBar
